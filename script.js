f8d68f614c26fd2af72383510488a9909330c66d
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-02-22 08:50:23 +0100

    add mergeSort function

--- script.js
+++ script.js
@@ -10,8 +10,60 @@ function createNode(data){
 let node = createNode("Fried", 1, 2)
 
 let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-let sortedArr = [1, 2, 3, 4, 5, 6, 7, 8]
+let sortedArr = mergeSort(arr)
 let smallArr = [1,2]
+
+
+
+function mergeSort(array){
+    // base case
+    if(array.length<2){
+        return array
+    }
+    else{
+        // divide the array into left and right half
+        let arr1 = array.splice(0, Math.floor(array.length/2))
+        let arr2 = array
+
+        // compare the two array items
+        let left = mergeSort(arr1)
+        let right = mergeSort(arr2)
+
+        // start merging the arrays after the base case has been reached
+        let merged = [];
+        let totalLength = left.length + right.length
+
+        for(let i=0; i<totalLength; i++){
+            if(left[0]<right[0]){
+                // move item to merged array
+                merged.push(left[0])
+                // remove item from left array
+                left.shift()
+                if(left.length<1){
+                    // push whole array to merged
+                    for(let r=0; r<right.length; r++){
+                        merged.push(right[r])
+                        i++
+                    }
+                }
+            }
+            else if(left[0]>right[0]){
+                // move item to merged array
+                merged.push(right[0])
+                // remove item from left array
+                right.shift()
+                if(right.length<1){
+                    // push whole array to merged
+                    for(let l=0; l<left.length; l++){
+                        merged.push(left[l])
+                        i++
+                    }
+                }
+            }
+        }
+        return merged
+    }
+}
 
 function sortedBinaryTree(array, start, end){
     // base cases: 
